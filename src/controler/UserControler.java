15316da902b59bdf67f668b8ef679da5e889f438
lparet <lparet@yseop.com>
2017-05-24 17:36:54 +0200

    Add Warn when connexion refused

--- src/controler/UserControler.java
+++ src/controler/UserControler.java
@@ -1,9 +1,12 @@
 package controler;
 
+import java.util.Calendar;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import beans.UserBean;
 import dao.UserDao;
@@ -45,10 +48,15 @@ public class UserControler {
 		if(user != null) {
 			return "activities.jsp";
 		} else {
-			
-			return "login.jsp";
+			addMessage("Connexion Refusée");
+			return null;
 		}
 	}
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_WARN, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
 
 	/**
 	 * To test
@@ -56,14 +64,14 @@ public class UserControler {
 	 */
 	public static void main(String[] args) {
 		UserControler uc = new UserControler();
-//		UserBean user = new UserBean("louis", "louis", "louis", "paret", new java.sql.Date(Calendar.getInstance().getTime().getTime()), "louis.paret@gmail.com");
-//		boolean addUser = uc.addUser(user);
-//		System.out.println("User add: " + addUser);
+		UserBean user = new UserBean("louis", "louis", "louis", "paret", new java.sql.Date(Calendar.getInstance().getTime().getTime()), "louis.paret@gmail.com");
+		boolean addUser = uc.addUser(user);
+		System.out.println("User add: " + addUser);
 
-//		List<UserBean> allUsers = uc.getAllUsers();
-//		for(UserBean u : allUsers) {
-//			System.out.println(u.getFirstname());
-//		}
+		List<UserBean> allUsers = uc.getAllUsers();
+		for(UserBean u : allUsers) {
+			System.out.println(u.getFirstname());
+		}
 		
 	}
 }
