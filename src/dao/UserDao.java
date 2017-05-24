9bf48cef365679c2d9a9588855a8464c8797e9ac
lparet <lparet@yseop.com>
2017-05-24 11:42:11 +0200

    Add UserBean

--- src/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import beans.UserBean;
-
-public interface UserDao {
-    void creer(UserBean user) throws DAOException;
-
-    UserBean trouver(String email) throws DAOException;
-}
